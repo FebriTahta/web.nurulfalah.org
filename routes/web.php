0635bc0f4e1ad871cf2947f3cc396f5007bb00fb
FebriTahta <febrirtah@gmail.com>
2022-03-06 19:26:47 -0800

    oe

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\landingController;
+use App\Http\Controllers\LandingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
